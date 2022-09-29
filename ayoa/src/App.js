4d0aab911972cc013e1870f951521f69161b0b26
satyam0337 <satyam>
2022-09-28 20:26:12 -0700

    added

---
+++ ayoa/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './Components/Home';
+// import WithSubnavigation from './Components/Navbar';
+
+function App() {
+  return (
+
+      <Home/>
+
+  );
+}
+
+export default App;
