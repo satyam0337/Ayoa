b342964d4001caa1e8da589b927d3c32f709e29c
satyam0337 <satyam>
2022-09-29 22:01:24 -0700

    added

--- ayoa/src/Components/Home.jsx
+++ ayoa/src/Components/Home.jsx
@@ -1,24 +1,80 @@
-import { Stack , Text} from '@chakra-ui/react'
+import { Stack, Text, Box, Image, Container, Center } from '@chakra-ui/react'
 import React from 'react'
-import WithSubnavigation from './Navbar' 
+import WithSubnavigation from './Navbar'
+import Getstarted from './Getstarted'
+import Imgvideo from './Imgvideo'
 const Home = () => {
   return (
-    <div>
-        
-        <WithSubnavigation/>
-    <Stack dir='column' alignItems={"center"} m={"-2"} > 
-        <Text   fontSize='6xl' fontWeight={'900'} >
-        Be different, work visually
+    <Box alignItems="center"  >
+
+      <WithSubnavigation />
+
+      <Box dir='column' textAlign={"center"}  >
+        <Text fontSize='6xl' fontWeight={'900'} lineHeight={1} >
+          Be different, work visually
         </Text>
-        <Text  fontSize={"xl"} fontWeight={"thin"} > Try the world’s most visual productivity app. Supercharge thinking, planning and</Text>
-        <Text fontSize={"xl"} fontWeight={"thin"}  > learning with AI, mind maps, task boards, and more </Text>
+        <Box>
+
+          <Box w="50%" m={"auto"} > <Text fontSize={'xl'} mt={'8'} textAlign={'center'} fontWeight={"thin"}  > Try the world’s most visual productivity app. Supercharge thinking, planning and learning with AI, mind maps, task boards, and more </Text> </Box>
+
+        </Box>
 
 
-    </Stack>
-    
-   
-    
-    </div>
+      </Box>
+
+      <  Getstarted />
+
+      <Stack alignItems={"center"}>
+        <Image src={"https://www.ayoa.com/wp-content/uploads/2022/07/ayoa_allinone_watch.webp"} mt="4" alt="video-pic" />
+
+      </Stack>
+      <Imgvideo />
+
+
+{/* Card Section  */}
+
+      <Center alignItems={"center"} w={{lg:'5xl',xl:'5xl',md:'3xl',sm:'3xl'}} m="auto" mt={'10'} dir={{md:'row',xl:'row'}}>
+        <Stack alignItems={"center"} gap="5">
+          <Box>
+            <Image src='https://www.ayoa.com/wp-content/uploads/2018/05/cbsnews_logo.png' alt="cardpic" />
+          </Box>
+          <Box textAlign={"center"} >
+            <Text fontSize={"16px"} fontWeight={"thin"} fontFamily={"sans-serif"}  >“The most unique twist on task management I’ve ever seen” </Text>
+
+          </Box>
+        </Stack >
+        <Stack alignItems={"center"} gap="5">
+          <Box>
+            <Image src='https://www.ayoa.com/wp-content/uploads/2018/05/techcrunch-logo.png' alt="cardpic" />
+          </Box>
+          <Box textAlign={"center"} >
+            <Text fontSize={"16px"} fontWeight={"thin"} fontFamily={"sans-serif"} >“Visually-oriented people – this layout boosts productivity” </Text>
+
+          </Box>
+        </Stack >
+        <Stack alignItems={"center"} gap="5">
+          <Box>
+            <Image src='https://www.ayoa.com/wp-content/uploads/2019/09/logo-lhacker.png' alt="cardpic" />
+          </Box>
+          <Box textAlign={"center"} >
+            <Text fontSize={"16px"} fontWeight={"thin"} fontFamily={"sans-serif"} >“Turns your tasks into an awesome venn diagram” </Text>
+
+          </Box>
+        </Stack >
+        <Stack alignItems={"center"} gap="5">
+          <Box>
+            <Image src='https://www.ayoa.com/wp-content/uploads/2019/09/logo-pcworld.png' alt="cardpic" />
+          </Box>
+          <Box textAlign={"center"} >
+            <Text fontSize={"16px"} fontWeight={"thin"} fontFamily={"sans-serif"} >“A more intuitive way to manage workloads” </Text>
+
+          </Box>
+        </Stack >
+
+      </Center>
+
+
+    </Box>
 
   )
 }
---
+++ ayoa/src/Components/Imgvideo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Imgvideo = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Imgvideo
